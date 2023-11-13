35a450247aad445903b79c4aed262bc2144a2f68
Marynryn <murmar95@gmail.com>
2023-11-13 13:42:44 +0200

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,50 @@
-export const App = () => {
+import React from "react"
+import Form from "../components/Form/Form"
+import Filter from "./Filter/Filter";
+import ListContacts from "./ListContacts/ListContacts";
+export class App extends React.Component {
+ 
+  state = {
+    contacts: [],
+    filter: ''
+  }
+
+changeFilter =(event)=>{
+  this.setState({filter:event.target.value})
+}
+handleSubmit = (newContact, filter) =>{
+ const{contacts}= this.state;
+ const isContactExists = contacts.some((contact) =>contact.name === newContact.name);
+ if(isContactExists) {
+alert(`${newContact.name} is already in contacts.`) }
+  this.setState((prevState)=>({contacts:[...prevState.contacts,newContact], filter: filter,}));
+  }
+  handleDelete= (contactId) =>{
+    this.setState((prevState) =>({contacts: prevState.contacts.filter((contact)=> contact.id !== contactId),}));
+  }
+  render(){
+   
+    
+    const contactsToLower = this.state.filter.toLowerCase();
+    const visualContacts = this.state.contacts ? this.state.contacts.filter((contact) =>contact.name && contact.name.toLowerCase().includes(contactsToLower)):[];
   return (
     <div
       style={{
+        padding:50,
         height: '100vh',
-        display: 'flex',
+        display: 'block',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
         color: '#010101'
       }}
     >
-      React homework template
+       <h1 style ={{fontSize: 48}}>Phonebook</h1>
+      <Form  onSubmit={this.handleSubmit} filter ={this.state.filter}/>
+
+<h2 style ={{fontSize: 46}}>Contacts</h2>
+<Filter filter={this.state.filter} onChange={this.changeFilter}/>
+<ListContacts props={this.state}  myContacts={visualContacts} onDelete={this.handleDelete}/>
     </div>
-  );
+  );}
 };
---
+++ src/components/ListContacts/ListContacts.jsx
@@ -0,0 +1,12 @@
+import Contact from "components/Contact/Contact";
+
+const ListContacts = (props) =>{
+
+    return(
+        <div className="contacts"> 
+<Contact props={props.myContacts} onDelete={props.onDelete}/>
+      
+        </div>
+    )
+}
+export default ListContacts;
