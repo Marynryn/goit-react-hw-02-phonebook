e071406e696122b846d7d16d37a43c36a57dbd75
Marynryn <murmar95@gmail.com>
2023-11-14 09:50:09 +0200

    mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,50 +1,77 @@
-import React from "react"
-import Form from "../components/Form/Form"
-import Filter from "./Filter/Filter";
-import ListContacts from "./ListContacts/ListContacts";
+import React from 'react';
+import Form from '../components/Form/Form';
+import Filter from './Filter/Filter';
+import ListContacts from './ListContacts/ListContacts';
+import { nanoid } from 'nanoid';
+
+
 export class App extends React.Component {
- 
   state = {
     contacts: [],
-    filter: ''
-  }
+    filter: '',
+  };
 
-changeFilter =(event)=>{
-  this.setState({filter:event.target.value})
-}
-handleSubmit = (newContact, filter) =>{
- const{contacts}= this.state;
- const isContactExists = contacts.some((contact) =>contact.name === newContact.name);
- if(isContactExists) {
-alert(`${newContact.name} is already in contacts.`); } else{
-  this.setState((prevState)=>({contacts:[...prevState.contacts,newContact], filter: filter,}));
-  }}
-  handleDelete= (contactId) =>{
-    this.setState((prevState) =>({contacts: prevState.contacts.filter((contact)=> contact.id !== contactId),}));
-  }
-  render(){
-   
-    
+  changeFilter = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  handleSubmit = (name, number) => {
+    const { contacts } = this.state;
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    const isContactExists = contacts.some(
+      contact => contact.name === name
+    );
+    if (isContactExists) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+      }));
+    }
+  };
+
+  handleDelete = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+  filterContacts = () => {
     const contactsToLower = this.state.filter.toLowerCase();
-    const visualContacts = this.state.contacts ? this.state.contacts.filter((contact) =>contact.name && contact.name.toLowerCase().includes(contactsToLower)):[];
-  return (
-    <div
-      style={{
-        padding:50,
-        height: '100vh',
-        display: 'block',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-       <h1 style ={{fontSize: 48}}>Phonebook</h1>
-      <Form  onSubmit={this.handleSubmit} filter ={this.state.filter}/>
+    return this.state.contacts.filter(
+      contact =>
+        contact.name && contact.name.toLowerCase().includes(contactsToLower)
+    );
+  };
 
-<h2 style ={{fontSize: 46}}>Contacts</h2>
-<Filter filter={this.state.filter} onChange={this.changeFilter}/>
-<ListContacts props={this.state}  myContacts={visualContacts} onDelete={this.handleDelete}/>
-    </div>
-  );}
-};
+  render() {
+    const visualContacts = this.filterContacts();
+    return (
+      <div
+        style={{
+          padding: 50,
+          height: '100vh',
+          display: 'block',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h1 style={{ fontSize: 48 }}>Phonebook</h1>
+        <Form onSubmit={this.handleSubmit} />
+
+        <h2 style={{ fontSize: 46 }}>Contacts</h2>
+        <Filter filter={this.state.filter} onChange={this.changeFilter} />
+        <ListContacts
+          props={this.state}
+          myContacts={visualContacts}
+          onDelete={this.handleDelete}
+        />
+      </div>
+    );
+  }
+}
